78ef2cab8079c560e291091e11fd648b6307eb65
hnakahara <hnakahara@indeed.com>
2019-08-23 20:06:22 +0900

    Add commands

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,1 +1,6 @@
 cp git_config ~/.gitconfig
+
+sudo apt-get update -y
+sudo apt-get install dstat iperf git htop vim emacs iftop -y
+yes "" | bash <(curl -Ss https://my-netdata.io/kickstart.sh)
+echo '(define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))' > ~/.emacs
