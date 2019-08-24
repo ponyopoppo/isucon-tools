1bf99dc04448aceeda4cd5b390e631d05e613de1
Komine Shunta <kmnshnt@gmail.com>
2019-08-25 00:44:55 +0900

    [Add] nginx-log-exporter

---
+++ nginx-log-exporter/index.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+const app = express();
+
+// const ACCESS_LOG_PATH = '/usr/local/Cellar/nginx/1.17.3/logs';
+const ACCESS_LOG_PATH = '/var/log/nginx/access.log';
+
+app.use((_, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS',
+    );
+    next();
+});
+
+app.get('/', (req, res) => {
+    const files = fs.readdirSync(ACCESS_LOG_PATH);
+    res.json({ files });
+});
+
+app.get('/:filename', (req, res) => {
+    const { filename } = req.params;
+    const buffer = fs.readFileSync(path.join(ACCESS_LOG_PATH, filename));
+    const content = buffer.toString('utf-8');
+    res.json({ content });
+});
+
+app.delete('/:filename', (req, res) => {
+    const { filename } = req.params;
+    fs.truncateSync(path.join(ACCESS_LOG_PATH, filename), 0);
+    res.json({ filename });
+});
+
+app.post('/copy/:from/:to', (req, res) => {
+    const { from, to } = req.params;
+    fs.copyFileSync(
+        path.join(ACCESS_LOG_PATH, from),
+        path.join(ACCESS_LOG_PATH, to),
+    );
+    res.json({ from, to });
+});
+
+app.listen(13030, () => {
+    console.log('listening on 13030');
+});
