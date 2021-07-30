b8d46ef93d91116c29981690fbc06f72ce009875
Hiroshi Nakahara <hiro116s@gmail.com>
2021-07-30 16:13:22 +0900

    Update scripts to post a URL instead of a file to Discord

--- bin/mysql-postprocess.sh
+++ bin/mysql-postprocess.sh
@@ -1,6 +1,6 @@
-if [ $# -ne 1 ]; then
-    echo "Usage: `basename $0` {server_id}" 1>&2
-    echo "Example: `basename $0` server1" 1>&2
+if [ $# -ne 2 ]; then
+    echo "Usage: `basename $0` {server_id} {server_host}" 1>&2
+    echo "Example: `basename $0` server1 127.0.0.1:39591" 1>&2
     exit 1
 fi
 
@@ -9,20 +9,36 @@ if [ -z "$DISCORD_WEBHOOK_URL" ]; then
     exit 1
 fi
 
-sudo ls /var/log/mysql/mysql-slow.log > /dev/null
+(sudo ls /var/log/mysql/mysql-slow.log 2>&1) > /dev/null
 if [ $? -ne 0 ]; then
-    echo "There is no such file: /var/log/mysql/mysql-slow.log"
+    echo "There is no such file: /var/log/mysql/mysql-slow.log" 1>&2
+    exit 1
+fi
+
+(which pt-query-digest 2>&1) > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Please install pt-query-digest first by following https://www.notion.so/MySQL-e88178f037fe43bbabdca94de605bbc6#09854e50b3b049cba1d667f094294b71" 1>&2
     exit 1
 fi
 
 now=$(date "+%Y%m%d-%H%M%S")
-tmp_local_file=mysqlslow-$now-$1-$server_id.txt
+server_host=$2
 
-cat <<EOF > $tmp_local_file
+mysqlslow_log_file=mysqlslow-$now-$1.txt
+mysqlslow_url=http://${server_host}/${mysqlslow_log_file}
+ptquerydigest_log_file=ptquerydigest-$now-$1.txt
+ptquerydigest_url=http://${server_host}/${ptquerydigest_log_file}
+
+cat <<EOF > $mysqlslow_log_file
 $(sudo mysqldumpslow /var/log/mysql/mysql-slow.log -s t)
 EOF
 
-curl -X POST -F "file=@${tmp_local_file}" ${DISCORD_WEBHOOK_URL}
-rm $tmp_local_file
+cat <<EOF > $ptquerydigest_log_file
+$(sudo pt-query-digest /var/log/mysql/mysql-slow.log)
+EOF
+
+sudo mv $mysqlslow_log_file $ptquerydigest_log_file "/var/log/isucon"
+curl -X POST -H "Content-Type: application/json" -d "{\"content\":\"slowdump: ${mysqlslow_url}\npt-query-digest:${ptquerydigest_url}\"}" ${DISCORD_WEBHOOK_URL}
+
 sudo mv /var/log/mysql/mysql-slow.log "/var/log/mysql/mysql-slow-${now}.log"
 sudo systemctl restart mysql
--- bin/spec.sh
+++ bin/spec.sh
@@ -1,6 +1,6 @@
-if [ $# -ne 1 ]; then
-    echo "Usage: `basename $0` {server_id}" 1>&2
-    echo "Example: `basename $0` server1" 1>&2
+if [ $# -ne 2 ]; then
+    echo "Usage: `basename $0` {server_id} {server_host}" 1>&2
+    echo "Example: `basename $0` server1 127.0.0.1:39591" 1>&2
     exit 1
 fi
 
@@ -9,20 +9,24 @@ if [ -z "$DISCORD_WEBHOOK_URL" ]; then
     exit 1
 fi
 
-tmp_local_file=$1.txt
+server_host=$2
 
-cat << EOF > $tmp_local_file
+spec_log_file=spec-$1.txt
+spec_url=http://${server_host}/${spec_log_file}
+service_log_file=services-$1.txt
+service_url=http://${server_host}/${service_log_file}
+
+cat << EOF > $spec_log_file
 <<$1>>
 $(lscpu | egrep "(Architecture|^CPU\(s\):|^Model name|Flags)" | sed -e "s/: */: /g")
 $(lsmem | grep "Total online" | sed -e "s/Total online memory:      /Memory:/g")
 $(lsblk | grep disk | cut -d' ' -f12 | sed -e "s/^/Disk: /g")
 EOF
-curl -X POST -F "file=@${tmp_local_file}" ${DISCORD_WEBHOOK_URL}
 
-cat << EOF > $tmp_local_file
+cat << EOF > $service_log_file
 <<$1>>
 $(systemctl list-unit-files --type=service)
 EOF
 
-curl -X POST -F "file=@${tmp_local_file}" ${DISCORD_WEBHOOK_URL}
-rm $tmp_local_file
+sudo mv $spec_log_file $service_log_file "/var/log/isucon"
+curl -X POST -H "Content-Type: application/json" -d "{\"content\":\"spec: ${spec_url}\nservices:${service_url}\"}" ${DISCORD_WEBHOOK_URL}
