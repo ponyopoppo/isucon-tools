cba8078e6c84669788e68498baaf75a7f77557cd
Komine Shunta <kmnshnt@gmail.com>
2023-11-08 13:17:51 +0800

    [Update] bootstrap node.js version 20

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -46,16 +46,19 @@ install_netdata() {
 
 install_node_js() {
     sudo node -v || echo 'node is not installed'
-    read -p "Install node 14? (y/N): " yn
+    NODE_MAJOR=20
+    read -p "Install node $NODE_MAJOR? (y/N): " yn
     case "$yn" in
     [yY]*) echo Installing ;;
     *) return ;;
     esac
-    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-    curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt update -y
-    sudo apt install nodejs yarn build-essential -y
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt-get update -y
+    sudo apt-get install nodejs yarn build-essential -y
 }
 
 install_ts_node() {
