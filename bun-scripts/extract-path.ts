c3d9fb47d4b439e1d124f5c4b8855ed61abb8bb5
Komine Shunta <kmnshnt@gmail.com>
2023-11-20 12:33:02 +0800

    [Update] extract-path

--- bun-scripts/extract-path.ts
+++ bun-scripts/extract-path.ts
@@ -3,55 +3,36 @@ import ts from 'typescript';
 
 interface Config {
     endpointMethodNames: string;
-    endpointAppName: string;
 }
 
 function findEndpoints(
     node: ts.Node,
     sourceFile: ts.SourceFile,
-    config: Config
+    config: Config,
+    endpoints: { [key: string]: string[] } = {}
 ) {
-    let endpoints: string[] = [];
     ts.forEachChild(node, (n) => {
-        endpoints = [...endpoints, ...findEndpoints(n, sourceFile, config)];
+        findEndpoints(n, sourceFile, config, endpoints);
     });
     if (
         !ts.isCallExpression(node) ||
         !ts.isPropertyAccessExpression(node.expression)
     ) {
-        return endpoints;
+        return;
     }
     const propAccess = node.expression;
     const name = propAccess.name.text;
     const instanceName = propAccess.expression.getText(sourceFile);
     if (!(config?.endpointMethodNames?.split(',') ?? []).includes(name)) {
-        return endpoints;
-    }
-    if (!(config?.endpointAppName?.split(',') ?? []).includes(instanceName)) {
-        return endpoints;
+        return;
     }
 
     const path = node.arguments[0]?.getText(sourceFile).split("'")[1];
-    endpoints = [...endpoints, path];
-    return endpoints;
-}
-
-function isTopLevelArrowFunction(node: ts.Node): boolean {
-    if (ts.isArrowFunction(node)) {
-        return true;
+    if (path) {
+        if (!endpoints[instanceName]) endpoints[instanceName] = [];
+        endpoints[instanceName].push(path);
     }
-    if (!ts.isVariableStatement(node)) {
-        return false;
-    }
-    const declaration = node.declarationList.declarations[0];
-    if (!declaration || !ts.isVariableDeclaration(declaration)) {
-        return false;
-    }
-    const initializer = declaration.initializer;
-    if (!initializer) {
-        return false;
-    }
-    return ts.isArrowFunction(initializer);
+    return;
 }
 
 function main() {
@@ -64,11 +45,22 @@ function main() {
     const sourceFile = ts.createSourceFile('app.ts', sourceText, {
         languageVersion: ts.ScriptTarget.ES2022,
     });
-    const endpoints = findEndpoints(sourceFile, sourceFile, {
-        endpointAppName: 'app,router',
-        endpointMethodNames: 'get,post,put,delete',
-    });
-    console.log(endpoints.join('\n'));
+    const endpoints: { [key: string]: string[] } = {};
+    findEndpoints(
+        sourceFile,
+        sourceFile,
+        {
+            endpointMethodNames: 'get,post,put,delete',
+        },
+        endpoints
+    );
+
+    for (const [instanceName, paths] of Object.entries(endpoints)) {
+        console.log(`==== ${instanceName} ====`);
+        for (const path of paths) {
+            console.log(path);
+        }
+    }
 }
 
 main();
